0b1a3cbf49eb771a2520e47a72f9a68abdd4fc4b
QingLiangYu1996 <56919073+QingLiangYu1996@users.noreply.github.com>
2019-11-20 21:20:53 +0800

    Update is_Change_word.py

--- is_Change_word.py
+++ is_Change_word.py
@@ -1,13 +1,13 @@
 #是否为变位词字典解法
 
 def is_Change_word(s1,s2):
-    s1_dict=dict()
-    s2_dict=dict()
-    for s1 in s1:
-        s1_dict[s1]=s1_dict.get(s1,0)+1
-    for s2 in s2:
-        s2_dict[s2]=s2_dict.get(s2,0)+1
-    if len(s1_dict)==len(s2_dict):
+    if len(s1)==len(s2):
+        s1_dict=dict()
+        s2_dict=dict()
+        for s1 in s1:
+            s1_dict[s1]=s1_dict.get(s1,0)+1
+        for s2 in s2:
+            s2_dict[s2]=s2_dict.get(s2,0)+1
         for key in s1_dict:
             if s1_dict[key]!=s2_dict.get(key,0):
                 return False
@@ -17,7 +17,7 @@ def is_Change_word(s1,s2):
 
 
 s1='python'
-s2='typhon'
+s2='avxsad'
 if is_Change_word(s1,s2):
     print ('True')
 else:
